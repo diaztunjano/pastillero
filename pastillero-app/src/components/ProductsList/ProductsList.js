1fa05626553516c09a702c751d116cf448dcd5ae
diaztunjano <dfdiaz1@uc.cl>
2022-07-20 01:12:43 -0400

    feat(product): add image url and resizing elements

--- pastillero-app/src/components/ProductsList/ProductsList.js
+++ pastillero-app/src/components/ProductsList/ProductsList.js
@@ -3,12 +3,25 @@ import Product from "../product/Product";
 import "../../styles/ProductsList.css";
 
 const ProductsList = () => {
+  const info = {
+    url: "https://d131ml7m6yr3wl.cloudfront.net/images/8632a5f3-546e-4a60-a4a0-55d7aaa8d8c6/large.jpeg",
+    name: "Eutirox",
+    concentration: "85 mg",
+    qty_left: 18,
+    days_left: 5,
+  };
   return (
     <div class="items">
       <div class="header-list">
         <h4 class="title">Te queda:</h4>
       </div>
-      <Product />
+      <Product
+        url={info.url}
+        name={info.name}
+        concentration={info.concentration}
+        qty_left={info.qty_left}
+        days_left={info.days_left}
+      />
     </div>
   );
 };
--- pastillero-app/src/components/product/Product.js
+++ pastillero-app/src/components/product/Product.js
@@ -2,22 +2,20 @@ import React from "react";
 import "../../styles/Product.css";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 
-const Product = () => {
+const Product = ({ url, name, concentration, qty_left, days_left }) => {
   return (
-    <div class="info">
-      <div class="image"></div>
-      <div class="text-info">
-        {/* <h5>${props.name}</h5>
-        <p>${props.info} mg</p>
-        <p>Quedan ${props.comps_remaining} comprimidos</p>
-        <p>Para ${props.days_left} días</p> */}
-        <h5>Eutirox</h5>
-        <p>75 mg</p>
-        <p class="underline">Quedan 5 comprimidos</p>
-        <p class="underline">Para 5 días</p>
+    <div class="product-info">
+      <div class="product-image">
+        <img src={url} alt={name} />
+      </div>
+      <div class="product-text-info">
+        <h4>{name}</h4>
+        <p>{concentration}</p>
+        <p class="underline">Quedan {qty_left} comprimidos</p>
+        <p class="underline">Para {days_left} días</p>
       </div>
       <div class="cart-icon">
-        <AddShoppingCartIcon/>
+        <AddShoppingCartIcon />
       </div>
     </div>
   );
